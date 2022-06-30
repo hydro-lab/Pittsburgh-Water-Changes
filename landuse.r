695a327a1fd25cb1d8f0541abd29bc3ab4428d7d
alannabattle <alanna.m.battle@gmail.com>
2022-06-30 11:42:45 -0400

    6/29/22 changes

--- landuse.r
+++ landuse.r
@@ -10,10 +10,54 @@ setwd("D:\\GIS\\layers")
 
 # ------------------------------------------------------------------------------
 # Import raster image
-cover <- raster("landlayers\\LCMAP_CU_2009_V12_LCPRI.tif") # Aug
+cover24 <- raster("landlayers\\LCMAP_CU_2009_V12_LCPRI.tif") 
 
 # To crop the image
-crp_ext <- readOGR("Chartier\\globalwatershed.shp")
+crp_ext24 <- readOGR("Chartier\\globalwatershed.shp")
+#renamed "cover" for 2009 to "cover25" to help numerically organize
+
+#cover35 <- raster("landlayer\\LCMAP_CU_2020_V12_LCPRI.tif")
+#cover34 <- raster("landlayer\\LCMAP_CU_2019_V12_LCPRI.tif")
+#cover33 <- raster("landlayer\\LCMAP_CU_2018_V12_LCPRI.tif")
+#cover32 <- raster("landlayer\\LCMAP_CU_2017_V12_LCPRI.tif")
+#cover31 <- raster("landlayer\\LCMAP_CU_2016_V12_LCPRI.tif")
+#cover30 <- raster("landlayer\\LCMAP_CU_2015_V12_LCPRI.tif")
+#cover29 <- raster("landlayer\\LCMAP_CU_2014_V12_LCPRI.tif")
+#cover28 <- raster("landlayer\\LCMAP_CU_2013_V12_LCPRI.tif")
+#cover27 <- raster(landlayer\\LCMAP_CU_2012_V12_LCPRI.tif")
+#cover26 <- raster("landlayer\\LCMAP_CU_2011_V12_LCPRI.tif")
+
+#cover25 <- raster("landlayer\\LCMAP_CU_2010_V12_LCPRI.tif")
+
+
+
+#cover23 <- raster("landlayers\\LCMAP_CU_2008_V12_LCPRI.tif")
+#crp_ext23 <- readOGR("Chartier\\globalwatershed.shp")
+#cover22 <- raster("landlayers\\LCMAP_CU_2007_V12_LCPRI.tif")
+
+#cover21 <- raster("landlayers\\LCMAP_CU_2006_V12_LCPRI.tif")
+#cover20 <- raster("landlayers\\LCMAP_CU_2005_V12_LCPRI.tif")
+#cover19 <- raster("landlayers\\LCMAP_CU_2004_V12_LCPRI.tif")
+#cover18 <- raster("landlayers\\LCMAP_CU_2003_V12_LCPRI.tif")
+#cover17 <- raster("landlayer\\LCMAP_CU_2002_V12_LCPRI.tif")
+#cover16 <- raster("landlayer\\LCMAP_CU_2001_V12_LCPRI.tif")
+#cover15 <- raster("landlayers\\LCMAP_CU_2000_V12_LCPRI.tif")
+#cover14 <- raster("landlayers\\LCMAP_CU_1999_V12_LCPRI.tif")
+#cover13 <- raster("landlayers\\LCMAP_CU_1998_V12_LCPRI.tif")
+#cover12 <- raster("landlayers\\LCMAP_CU_1997_V12_LCPRI.tif")
+#cover11 <- raster("landlayers\\LCMAP_CU_1996_V12_LCPRI.tif")
+#cover10 <- raster("landlayers\\LCMAP_CU_1995_V12_LCPRI.tif")
+#cover9 <- raster("landlayers\\LCMAP_CU_1994_V12_LCPRI.tif")
+#cover8 <- raster("landlayers\\LCMAP_CU_1993_V12_LCPRI.tif")
+#cover7 <- raster("landlayers\\LCMAP_CU_1992_V12_LCPRI.tif")
+#cover6 <- raster("landlayers\\LCMAP_CU_1991_V12_LCPRI.tif")
+#cover5 <- raster("landlayers\\LCMAP_CU_1990_V12_LCPRI.tif")
+#cover4 <- raster("landlayers\\LCMAP_CU_1989_V12_LCPRI.tif")
+#cover3 <- raster("landlayers\\LCMAP_CU_1988_V12_LCPRI.tif")
+#cover2 <- raster("landlayers\\LCMAP_CU_1987_V12_LCPRI.tif")
+#cover1 <- raster(landlayers\\LCMAP_CU_1986_V12_LCPRI.tif")
+#cover0 <- raster(landlayers\\LCMAP_CU_1985_V12_LCPRI.tif")
+
 
 sr<-"+proj=longlat +datum=WGS84"
 cover.proj<- projectRaster(cover,crs = sr)
