c2ac915a3d8029a63391f84495e5c62f22b88cf6
David Kahler <david.m.kahler@gmail.com>
2022-09-16 15:35:00 -0400

    removed resolution - wasn't needed

--- cutGeotiffs.py
+++ cutGeotiffs.py
@@ -13,15 +13,11 @@ ws = "/Volumes/T7/pa_hydro/Ohio/layers/globalwatershed.shp"
 
 # land cover dataset
 ls = glob.glob("/Volumes/T7/pa_hydro/landlayers/*.tif")
-# from the projected (QGIS) pixel size:
-dx = 0.0003253077953483678891
-dy = -0.0003253077953483678891
-# FOR: xRes = dx, yRes = dy, 
 
 # subset all image files to common outer coordinates
 for f in ls:
     split = f.split("_")
     wout = "/Volumes/T7/pa_hydro/Ohio/" + split[3] + ".crp.tif"
     gdal.Warp(wout, f, srcSRS = 'EPSG:5070', dstSRS = 'EPSG:4326', cutlineDSName = ws, cropToCutline=True)
-    print(str(split[0]))
+    print(str(split[3]))
 
